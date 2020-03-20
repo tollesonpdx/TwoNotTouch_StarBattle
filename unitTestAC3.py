9351cb1bdd4909851c2678922145dc9fb92598d2
WilliamMass <wamass@pdx.edu>
2020-03-19 18:48:13 -0700

    working through constraints

--- unitTestAC3.py
+++ unitTestAC3.py
@@ -8,21 +8,34 @@ class ac3_test(unittest.TestCase):
     This works with any subset of boardList or with a hard coded board
     '''
 
-
-    def test_board(self):
-        board = boardList.a
-        #  board = boardList.b
+    def test_vars(self):
+        # board = boardList.a
+        board = boardList.b
         Testac_CSP = ac_csp(board)
-        vars = Testac_CSP.get_vars(board)
-        arcs = Testac_CSP.make_arcs()
-        domains = []
-        constraints = {}
-        expected = len(board) * len(board)
+        vars = Testac_CSP.vars
+        expected = len(board) * 2  # we would have to hard code this to 1 to make it pass a test for 1 star
         result = len(vars)
         self.assertEqual(expected, result)
-        # print("Created: ", len(vars), "coordinates")
-        # print(vars)
-        expected = (math.pow(len(vars), 2) - len(vars))
+        # print("This has generated a list of" int(len(vars))
+
+    def test_domain(self):
+        # board = boardList.a
+        board = boardList.b
+        Testac_CSP = ac_csp(board)
+        domain = Testac_CSP.domain
+        expected = len(board) * len(board)
+        result = len(domain)
+        self.assertEqual(expected, result)
+        # print("This has generated", len(domain), "coordinates.  It should generate", int(math.pow(len(board), 2), "coordinates."
+        # print(domain)
+
+    def test_arcs(self):
+        # board = boardList.a
+        board = boardList.b
+        Testac_CSP = ac_csp(board)
+        domain = Testac_CSP.domain
+        arcs = Testac_CSP.arcs
+        expected = (math.pow(len(domain), 2) - len(domain))
         result = len(arcs)
         self.assertEqual(expected, result)
         # print("This has generated", len(arcs), "arcs.  It should generate", int(math.pow(len(vars), 2) - len(vars)) , "arcs.")

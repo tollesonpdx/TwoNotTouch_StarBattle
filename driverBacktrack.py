715a6f99781b58dfca0f4743f67a10af23f6501c
ejdeposit <edeposit@pdx.edu>
2020-03-20 17:52:11 -0700

    added boards to solve

--- driverBacktrack.py
+++ driverBacktrack.py
@@ -1,7 +1,8 @@
 import backTrack as bt
 from collections import namedtuple
+import tnt_board as tb
 
-board = ((1, 2, 2, 2, 2, 2, 2, 2, 2),
+easy1 = ((1, 2, 2, 2, 2, 2, 2, 2, 2),
         (1, 2, 2, 2, 2, 4, 4, 4, 2),
         (1, 3, 3, 3, 3, 4, 4, 4, 2),
         (1, 3, 5, 3, 5, 4, 6, 4, 9),
@@ -11,13 +12,17 @@ board = ((1, 2, 2, 2, 2, 2, 2, 2, 2),
         (1, 1, 7, 7, 8, 8, 8, 8, 8),
         (1, 1, 1, 7, 8, 8, 8, 8, 8))
 
+easyBoardObj = tb.board(easy1)
 
-
-csp = bt.CSP(board)
+csp = bt.CSP(easy1)
 solution= csp.backtracking_search()
-print(solution.vals.values())
+#print(solution.vals.values())
+print('Easy 1')
+easyBoardObj.print_board(solution.vals.values())
+solution=None
 
-medium = ((1,1,1,2,2,3,3,3,3),
+
+easy2 = ((1,1,1,2,2,3,3,3,3),
 (1,1,1,1,2,3,3,3,3),
 (1,4,4,1,2,3,5,5,3),
 (1,1,4,1,2,3,3,5,3),
@@ -26,11 +31,63 @@ medium = ((1,1,1,2,2,3,3,3,3),
 (1,4,4,7,7,7,5,5,3),
 (8,8,8,7,7,7,9,9,9),
 (8,8,8,7,7,7,9,9,9))
+easy2BoardObj = tb.board(easy2)
+cspEasy2 = bt.CSP(easy2)
+solution= cspEasy2.backtracking_search()
+print('Easy 2')
+easy2BoardObj.print_board(solution.vals.values())
+solution=None
 
-cspMed = bt.CSP(medium)
-solutionMed= cspMed.backtracking_search()
-print(solutionMed.vals.values())
+easy3= ((1,1,1,1,1,1,1,2,2,3),
+(1,4,1,1,5,2,2,2,3,3),
+(1,4,1,1,5,2,2,2,2,3),
+(4,4,1,5,5,6,6,6,6,6),
+(4,4,1,5,5,6,7,7,7,6),
+(8,9,1,1,1,6,0,0,0,6),
+(8,9,9,9,6,6,0,0,6,6),
+(8,9,9,0,6,6,0,0,6,6),
+(8,9,9,0,0,0,0,0,0,0),
+(8,9,9,9,0,0,0,0,0,0))
+easy3BoardObj = tb.board(easy3)
+cspEasy3=bt.CSP(easy3)
+solution=cspEasy3.backtracking_search()
+print('Easy 3')
+easy3BoardObj.print_board(solution.vals.values())
 
+med1= ((1,1,1,2,2,2,2,2,3,3),
+(1,1,1,2,4,4,4,4,3,3),
+(1,1,2,2,5,4,4,3,3,3),
+(1,6,2,5,5,5,4,3,3,3),
+(1,6,2,2,5,4,4,8,8,8),
+(6,6,2,2,2,7,4,9,9,8),
+(6,6,2,2,7,7,7,9,8,8),
+(6,6,2,2,2,7,9,9,9,8),
+(6,6,2,0,0,0,0,9,9,8),
+(6,0,0,0,0,0,0,0,9,9))
+med1BoardObj = tb.board(med1)
+cspMed1 = bt.CSP(med1)
+solution= cspMed1.backtracking_search()
+print('Medium 1')
+med1BoardObj.print_board(solution.vals.values())
+
+hard1=((1,2,2,2,2,2,2,2,2,3),
+(1,2,4,4,2,2,5,5,2,3),
+(1,2,4,4,4,5,5,5,2,3),
+(1,2,2,4,4,5,5,3,3,3),
+(1,1,2,6,6,7,7,3,3,3),
+(1,1,6,6,6,7,7,7,8,3),
+(1,1,6,6,9,9,7,7,8,3),
+(1,9,9,9,9,9,9,8,8,8),
+(1,9,9,9,9,9,9,9,8,8),
+(9,9,9,9,9,9,9,9,8,8))
+hard1BoardObj= tb.board(hard1)
+cspHard1 = bt.CSP(hard1)
+solution = cspHard1.backtracking_search()
+print('hard 1')
+hard1BoardObj.print_board(solution.vals.values())
+
+
+print('hard 2')
 extraHard= ((1,1,2,2,2,2,3,3,3,3),
        (1,1,4,4,2,2,3,5,5,3),
        (1,4,4,6,6,2,5,5,5,5),
